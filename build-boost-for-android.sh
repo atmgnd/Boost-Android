905806b0d02d4233649891f7c19f0f470ea2e13c
atmgnd <atmgnd@outlook.com>
2020-04-23 19:41:49 +0800

    修正与改进

--- build-boost-for-android.sh
+++ build-boost-for-android.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
-
-version=1.61.0
-echo "Building boost $version..."
+# export PATH=${HOME}/opt/android-ndk-r20/toolchains/llvm/prebuilt/linux-x86_64/bin:${PATH}
+# XARCH= # "x86" "ia64" "sparc" "power" "mips1" "mips2" "mips3" "mips4" "mips32" "mips32r2" "mips64" "parisc" "arm" "combined" "combined-x86-power"
 
 set -eu
 
-prefix=`pwd`/build
-export PATH=${HOME}/opt/android-ndk-r20/toolchains/llvm/prebuilt/linux-x86_64/bin:${PATH}
+# 修改以下值为合适的值
 export AR=arm-linux-androideabi-ar
 export AS=arm-linux-androideabi-as
-export CC=1rmv7a-linux-androideabi9-clang
+export CC=armv7a-linux-androideabi19-clang
 export CXX=armv7a-linux-androideabi19-clang++
 export LD=arm-linux-androideabi-ld
 export RANLIB=arm-linux-androideabi-ranlib
 export STRIP=arm-linux-androideabi-strip
 
-dir_name=boost_$(sed 's#\.#_#g' <<< $version)
+# 以下两个值可以随意命名, toolset 按此名的组合到user-config.jam中查找
+XCOMPILER=clang
+XPLATFORM=android
+XARCH=
+
+version=1.61.0
+version2=$(sed 's#\.#_#g' <<< $version)
+echo "Building boost $version..."
+
+prefix=`pwd`/sysroot
+
+dir_name=boost_${version2}
 archive=${dir_name}.tar.bz2
 if [ ! -f "$archive" ]; then
-  wget -O $archive "https://dl.bintray.com/boostorg/release/$version/source/$archive"
+  wget -O $archive "http://downloads.sourceforge.net/boost/$archive"
 else
   echo "Archive $archive already downloaded"
 fi
@@ -31,30 +40,29 @@ else
   echo "Archive $archive already unpacked into $dir_name"
 fi
 
-rm -f boost_1_61_0/libs/filesystem/src/operations.cpp
-cp operations-1.61.0.cpp boost_1_61_0/libs/filesystem/src/operations.cpp
+rm -f boost_${version2}/libs/filesystem/src/operations.cpp
+cp operations-${version}.cpp boost_${version2}/libs/filesystem/src/operations.cpp
 cd $dir_name
 
 echo "Generating config..."
+# 也可以通过覆盖 project-config.jam
 user_config=tools/build/src/user-config.jam
 rm -f $user_config
 cat > $user_config <<EOF
 import os ;
+import option ;
 
-using clang : android
-:
-"$CXX"
-:
-<archiver>$AR
-<ranlib>$RANLIB
-;
+using ${XCOMPILER} : ${XPLATFORM} : $CXX ;
+option.set keep-going : false ;
 EOF
 
+# using ${XCOMPILER} : target : $CXX : <archiver>$AR <ranlib>$RANLIB ;
+
 echo "Bootstrapping..."
-./bootstrap.sh #--with-toolset=clang
+./bootstrap.sh #--with-toolset=${XCOMPILER}
 
 echo "Building..."
-./b2 -j4 \
+B2_CMD="./b2 -j4 \
     --prefix=${prefix} \
     --with-atomic \
     --with-chrono \
@@ -81,15 +89,21 @@ echo "Building..."
     --with-type_erasure \
     --with-wave \
     -s NO_BZIP2=1 \
-    toolset=clang-android \
-    architecture=arm \
+    toolset=${XCOMPILER}-${XPLATFORM} \
     variant=release \
-    target-os=android \
     threading=multi \
     threadapi=pthread \
     link=static \
-    install
+    install"
 
+if test "${XARCH}" != ""
+then
+	B2_CMD="${B2_CMD} architecture=${XARCH}"
+fi
+
+${B2_CMD}
+
+#    target-os=android \
 #    --layout=versioned \
 #    runtime-link=static \
 #    --with-fiber \
