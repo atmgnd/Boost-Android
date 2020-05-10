aebd28ba98995ae4828e973320adfe3aa2312a5f
atmgnd <atmgnd@outlook.com>
2020-05-10 11:32:41 +0800

    添加1.68.0版本
    Signed-off-by: atmgnd <atmgnd@outlook.com>

---
+++ env_android_clang.sh
@@ -0,0 +1,14 @@
+AR=arm-linux-androideabi-ar
+AS=arm-linux-androideabi-as
+CC=armv7a-linux-androideabi19-clang
+CXX=armv7a-linux-androideabi19-clang++
+LD=arm-linux-androideabi-ld
+RANLIB=arm-linux-androideabi-ranlib
+STRIP=arm-linux-androideabi-strip
+
+# 以下两个值可以随意命名, toolset 按此名的组合到user-config.jam中查找
+XCOMPILER=clang
+XPLATFORM=android
+XARCH=
+
+version=1.61.0
---
+++ env_linux_gcc.sh
@@ -0,0 +1,14 @@
+AR=ar
+AS=as
+CC=gcc
+CXX=g++
+LD=ld
+RANLIB=ranlib
+STRIP=strip
+
+# 以下两个值可以随意命名, toolset 按此名的组合到user-config.jam中查找
+XCOMPILER=gcc
+XPLATFORM=linux
+XARCH=
+
+version=1.61.0
